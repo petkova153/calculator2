ed1fe73712977a5ff20f0dea420e1c85dd8ad1f4
petko <petkova153@gmail.com>
2023-03-08 20:30:31 +0200

    correct typos

--- src/com/sda/she_likes_java/numbers/IntegerRanges.java
+++ src/com/sda/she_likes_java/numbers/IntegerRanges.java
@@ -3,13 +3,13 @@ package com.sda.she_likes_java.numbers;
 public class IntegerRanges {
     public static void main(String[] args) {
        //see how integer looks like
-        System.out.println("See how inegers look like");
-        System.out.println("Ranges");
+        System.out.println("See how integers look like");
+        System.out.println("Ranges for integers");
         System.out.println("Min number is " + Integer.MIN_VALUE);
         System.out.println("Max number is " + Integer.MAX_VALUE);
 
         System.out.println("See how long look like");
-        System.out.println("Ranges");
+        System.out.println("Ranges for long");
         System.out.println("Min number is " + Long.MIN_VALUE);
         System.out.println("Max number is " + Long.MAX_VALUE);
     }
