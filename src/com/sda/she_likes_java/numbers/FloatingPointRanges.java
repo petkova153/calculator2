8148cce428fa96e127fe0c1218ab76cdd1e3c739
petko <petkova153@gmail.com>
2023-03-08 21:01:37 +0200

    floats and double

---
+++ src/com/sda/she_likes_java/numbers/FloatingPointRanges.java
@@ -0,0 +1,13 @@
+package com.sda.she_likes_java.numbers;
+
+public class FloatingPointRanges {
+    public static void main(String[] args) {
+        System.out.println("Floats:");
+        System.out.println("Min float nr is: " + Float.MIN_VALUE);
+        System.out.println("Max float nr is: " + Float.MAX_VALUE);
+
+        System.out.println("Doubles:");
+        System.out.println("Min double nr is: " + Double.MIN_VALUE);
+        System.out.println("Max double nr is: " + Double.MAX_VALUE);
+    }
+}
