31a191d83514a3b34f771cdaca6671b3af8d71bc
petko <petkova153@gmail.com>
2023-03-08 20:28:15 +0200

    integers

---
+++ src/com/sda/she_likes_java/numbers/IntegerRanges.java
@@ -0,0 +1,11 @@
+package com.sda.she_likes_java.numbers;
+
+public class IntegerRanges {
+    public static void main(String[] args) {
+       //see how integer looks like
+        System.out.println("See how inegers look like");
+        System.out.println("Ranges");
+        System.out.println("Min number is " + Integer.MIN_VALUE);
+        System.out.println("Max number is " + Integer.MAX_VALUE);
+    }
+}
