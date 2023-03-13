104d2566445f712df2e98c549cb3e6cfe0b1b667
petko <petkova153@gmail.com>
2023-03-13 20:08:11 +0200

    kid age while loop

---
+++ src/com/sda/she_likes_java/loops/while_loop/WhileGrow.java
@@ -0,0 +1,14 @@
+package com.sda.she_likes_java.loops.while_loop;
+
+public class WhileGrow {
+    public static void main(String[] args) {
+        int kidAge;
+        kidAge = 1;
+        while (kidAge <= 18)
+        {
+            System.out.println(kidAge);
+            kidAge++;
+        }
+        System.out.println("party time!");
+    }
+}
