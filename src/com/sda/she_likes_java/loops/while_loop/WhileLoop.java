84ee8faaae2b1bacc044f6bd47b6c1a453114397
petko <petkova153@gmail.com>
2023-03-13 20:04:02 +0200

    while looping

---
+++ src/com/sda/she_likes_java/loops/while_loop/WhileLoop.java
@@ -0,0 +1,14 @@
+package com.sda.she_likes_java.loops.while_loop;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i;
+
+        i = 0;
+        while (i <= 30)
+        {
+            System.out.println(i);
+        i++;
+        }
+    }
+}
