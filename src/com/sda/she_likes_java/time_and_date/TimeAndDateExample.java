cd3baa5bc545d981173a31146fc51e2f4c415bb0
petko <petkova153@gmail.com>
2023-03-19 11:07:49 +0200

    friday homework

--- src/com/sda/she_likes_java/time_and_date/TimeAndDateExample.java
+++ src/com/sda/she_likes_java/time_and_date/TimeAndDateExample.java
@@ -2,6 +2,7 @@ package com.sda.she_likes_java.time_and_date;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TimeAndDateExample {
     public static void main(String[] args) {
@@ -17,5 +18,8 @@ public class TimeAndDateExample {
         currentDateTime = LocalDateTime.now();
         System.out.println("Local date is: " + currentDateTime);
 
+        System.out.println("Let's format a bit.");
+        //System.out.println(currentDateTime.format(DateTimeFormatter.RFC_1123_DATE_TIME));
+        System.out.println(currentDateTime.format(DateTimeFormatter.BASIC_ISO_DATE));
     }
 }
