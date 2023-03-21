c7b2ce0ea713b5f0c9e4caa41fba75d706c5463e
petko <petkova153@gmail.com>
2023-03-21 22:00:42 +0200

    arrays exercise

---
+++ src/com/sda/she_likes_java/homework/Arrays.java
@@ -0,0 +1,14 @@
+package com.sda.she_likes_java.homework;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int i;
+        int[] arrayOfIx3;
+
+        arrayOfIx3 = new int[100];
+        for (i = 0; i <= 100; i++)
+        {
+            arrayOfIx3[i] = i*3;
+        }
+    }
+}
