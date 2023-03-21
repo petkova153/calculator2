f17d0de447d00515fde5390c9b6e2a4831331de8
petko <petkova153@gmail.com>
2023-03-21 22:14:31 +0200

    corrections

--- src/com/sda/she_likes_java/homework/Arrays.java
+++ src/com/sda/she_likes_java/homework/Arrays.java
@@ -6,9 +6,16 @@ public class Arrays {
         int[] arrayOfIx3;
 
         arrayOfIx3 = new int[100];
-        for (i = 0; i <= 100; i++)
+        for (i = 0; i <= 99; i++)
         {
             arrayOfIx3[i] = i*3;
+        }
+
+        System.out.println("your array is:\n");
+        System.out.println("with total length of " + arrayOfIx3.length);
+        for (i = 0; i <= 99; i++)
+        {
+            System.out.println("index nr " + i + ": " + arrayOfIx3[i]);
         }
     }
 }
