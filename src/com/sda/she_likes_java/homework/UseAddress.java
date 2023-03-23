c82ecdd02cd580c707d2cae303d487305cd1d61c
petko <petkova153@gmail.com>
2023-03-23 22:00:25 +0200

    methods and adresses

--- src/com/sda/she_likes_java/homework/UseAddress.java
+++ src/com/sda/she_likes_java/homework/UseAddress.java
@@ -4,8 +4,6 @@ public class UseAddress {
     public static void main(String[] args) {
         Address myAddress;
         Address recipientAddress;
-        String street;
-        String country;
 
         myAddress = new Address();
         recipientAddress = new Address();
@@ -15,6 +13,11 @@ public class UseAddress {
         myAddress.street = "Architektu";
         myAddress.postalCode = 4004;
 
+        recipientAddress.country = "Bulgaria";
+        recipientAddress.city = "Sofia";
+        recipientAddress.street = "Torpedo";
+        recipientAddress.postalCode = 1220;
+
         System.out.println("The address on the package I am sending will contain my address:\n" +
                 myAddress.country + "\n"+
                 myAddress.city + "\n" +
@@ -22,9 +25,9 @@ public class UseAddress {
                 myAddress.postalCode);
 
         System.out.println("And the recipient's address:\n" +
-                myAddress.country + "\n"+
-                myAddress.city + "\n" +
-                myAddress.street + "\n"+
-                myAddress.postalCode);
+                recipientAddress.country + "\n"+
+                recipientAddress.city + "\n" +
+                recipientAddress.street + "\n"+
+                recipientAddress.postalCode);
     }
 }
