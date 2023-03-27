406b001007ade2cb8e665a1860e0d4f07aa7043d
petko <petkova153@gmail.com>
2023-03-27 21:08:55 +0300

    explanation about static

--- src/com/sda/she_likes_java/classes/constructor/FullPerson.java
+++ src/com/sda/she_likes_java/classes/constructor/FullPerson.java
@@ -22,14 +22,26 @@ public class FullPerson {
                 ", address=" + address +
                 '}';
     }
-
+    public void describeYourself()
+    {
+        System.out.println("Hi I am " + name);
+    }
+    public static void genericDescription() {
+        // static is used even without an object
+        System.out.println("Generic description");
+    }
     public static void main(String[] args) {
         FullAddress addressNP;
 
         addressNP = new FullAddress("Portuga", "Lisbon", "Summer", "2021");
 
         FullPerson newPerson;
+        FullPerson personWithoutAddress;
         newPerson = new FullPerson("Pincess", "Snowwhite", 22, addressNP);
+        personWithoutAddress = new FullPerson("Mario", "JustMario", 35,null);
         System.out.println(newPerson);
+        newPerson.describeYourself();
+        personWithoutAddress.describeYourself();
+        genericDescription();
     }
 }
