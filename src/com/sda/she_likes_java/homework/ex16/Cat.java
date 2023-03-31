cca785ef6006e6c2501614671c32773d0b9dc4bd
petko <petkova153@gmail.com>
2023-03-31 19:08:16 +0300

    cat class

---
+++ src/com/sda/she_likes_java/homework/ex16/Cat.java
@@ -0,0 +1,24 @@
+package com.sda.she_likes_java.homework.ex16;
+
+public class Cat {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+}
