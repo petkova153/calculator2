93c84d33b728b7230df56988997117318cb7efd9
petko <petkova153@gmail.com>
2023-04-26 21:12:25 +0300

    casting and object

--- src/com/sda/she_likes_java/common_behaviour/wrong/Main.java
+++ src/com/sda/she_likes_java/common_behaviour/wrong/Main.java
@@ -1,5 +1,7 @@
 package com.sda.she_likes_java.common_behaviour.wrong;
 
+import kotlin.properties.ObservableProperty;
+
 public class Main {
     public static void main(String[] args) {
         Bird pigeon = new Bird();
@@ -10,5 +12,10 @@ public class Main {
         Flyable fParrot = new Parrot();
         commonParrot.go();
         fParrot.fly();
+        //we cast Abletoswim to sherchas since sherchas is an Animal and Animal can't swim
+        AbleToSwim scherchas2 = (AbleToSwim) sherchas;
+        scherchas2.swim();
+        Object objectTiger = new Tiger();
+        // cannot call go() and swim() - they are not visible here
     }
 }
