76c3b9cf2f22dd4d93e2dbc8384cd72c7e1bc37d
petko <petkova153@gmail.com>
2023-05-08 20:14:42 +0300

    collections list.of

--- src/com/sda/she_likes_java/collections/lists/Main.java
+++ src/com/sda/she_likes_java/collections/lists/Main.java
@@ -12,6 +12,13 @@ public class Main {
         pantalones.add(new Pants("levis", "jeans"));
         pantalones.add(new Pants("big star", "jeans"));
         ShowItems(pantalones);
+
+        //a way to create a collection without being able to add later
+        List<Pants> pantsishmantsi = List.of(
+                new Pants("levis", "jeans"),
+                new Pants("big star", "jeans"),
+                new Pants("bambina", "jeans"));
+        ShowItems(pantsishmantsi);
     }
 
     public static void ShowItems (List<Pants> items){
