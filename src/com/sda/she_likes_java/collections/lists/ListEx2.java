3fd1eee297f8f4468bd701e3d8ef130199596bbd
petko <petkova153@gmail.com>
2023-05-10 21:03:53 +0300

    lists nsme lists find person

--- src/com/sda/she_likes_java/collections/lists/ListEx2.java
+++ src/com/sda/she_likes_java/collections/lists/ListEx2.java
@@ -17,6 +17,13 @@ public class ListEx2 {
         printOnlySomeNames(names);
         addName(names);
         PrintNames(names);
+        List<Person> listPersons = List.of(
+                new Person("Hubba", "Bubba"),
+                new Person("Princess", "elsa"),
+                new Person("Pesho", "Goshov")
+        );
+        List<Person> foundPersons = findPersonsByName(listPersons, "Hubba");
+        System.out.println("Found name is: " +foundPersons);
     }
     public static void PrintNames(List<String> namePrints){
         System.out.println(namePrints);
@@ -34,6 +41,17 @@ public class ListEx2 {
     }
 
     public static List<Person> findPersonsByName(List<Person> persons, String nameToFind){
+        //create empty result
+        List<Person> result = new ArrayList<>();
+        //iterate through list
+        for (Person singlePerson: persons) {
+            if (singlePerson.getName().equals(nameToFind)){
+                result.add(singlePerson);
+                return result;
+            }
+        }
+        //if person has proper name add them to the result
+        //return the result
         return null;
     }
 }
