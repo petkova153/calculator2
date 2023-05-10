84ab28c32c16cb13245cae9d34a491153f566e35
petko <petkova153@gmail.com>
2023-05-10 20:10:46 +0300

    lists

--- src/com/sda/she_likes_java/collections/lists/ListEx2.java
+++ src/com/sda/she_likes_java/collections/lists/ListEx2.java
@@ -9,10 +9,20 @@ public class ListEx2 {
         names.add("Random");
         names.add("Bird");
         names.add("Bella");
+        names.add(2, "Bella D.");
         PrintNames(names);
+        printOnlySomeNames(names);
 
     }
     public static void PrintNames(List<String> namePrints){
         System.out.println(namePrints);
     }
+
+    public static void printOnlySomeNames(List<String> names) {
+        // starting from position number 1 - second item
+        for (int index = 1; index < names.size() - 1; index++) {
+            System.out.println("name is: " + names.get(index));
+        }
+    }
+
 }
