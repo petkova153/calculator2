73b5d9e732707a3319b71fc1637b1bd7cf61e9f3
petko <petkova153@gmail.com>
2023-05-31 21:31:42 +0300

    read all students

--- src/com/sda/she_likes_java/database_in_memory/AccentureStudentsInMemory.java
+++ src/com/sda/she_likes_java/database_in_memory/AccentureStudentsInMemory.java
@@ -1,10 +1,41 @@
 package com.sda.she_likes_java.database_in_memory;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AccentureStudentsInMemory {
+    private static final String selectAllStudents = """
+    SELECT ID, NAME,SURNAME,AGE, SEX
+    FROM STUDENTS;
+    """;
+    private Connection dbConn;
+    public AccentureStudentsInMemory(Connection dbConn) {
+        this.dbConn = dbConn;
+    }
+
     public List<AccentureStudent> getAllStudents() {
-        return List.of();
+        List<AccentureStudent> result = new ArrayList<>();
+        try {
+            Statement statement = dbConn.createStatement();
+            ResultSet recordsFromDb = statement.executeQuery(selectAllStudents);
+            while(recordsFromDb.next()){
+                Integer id = recordsFromDb.getInt("ID");
+                String name = recordsFromDb.getString("NAME");
+                String surname = recordsFromDb.getString("SURNAME");
+                int age = recordsFromDb.getInt("AGE");
+                String sex = recordsFromDb.getString("SEX");
+
+                AccentureStudent student = new AccentureStudent(id,name,surname,age,sex);
+                result.add(student);
+            }
+        } catch (SQLException e) {
+            System.out.println("Unexpected error: " + e);
+        }
+        return result;
     }
 
     public AccentureStudent getStudentById(Long id) {
--- src/com/sda/she_likes_java/database_in_memory/Main.java
+++ src/com/sda/she_likes_java/database_in_memory/Main.java
@@ -1,6 +1,8 @@
 package com.sda.she_likes_java.database_in_memory;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +10,13 @@ public class Main {
         System.out.println("Is connection present? " + (conneciton != null));
 
         DbInit.prepareData(conneciton);
+        System.out.println("Let's read all students");
+        AccentureStudentsInMemory repository = new AccentureStudentsInMemory(conneciton);
+        List<AccentureStudent> students = repository.getAllStudents();
+        System.out.println("Students are " + students);
+        for(AccentureStudent stUdent:students){
+            System.out.println(stUdent);
+
+        }
     }
 }
