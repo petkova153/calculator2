59f2efe3adf57af1c09555e2357b38f48b75ff8a
petko <petkova153@gmail.com>
2023-05-31 21:00:48 +0300

    SQL CRUD

---
+++ src/com/sda/she_likes_java/database_in_memory/AccentureStudentsInMemory.java
@@ -0,0 +1,25 @@
+package com.sda.she_likes_java.database_in_memory;
+
+import java.util.List;
+
+public class AccentureStudentsInMemory {
+    public List<AccentureStudent> getAllStudents() {
+        return List.of();
+    }
+
+    public AccentureStudent getStudentById(Long id) {
+        return null;
+    }
+
+    public AccentureStudent saveStudent(AccentureStudent student) {
+        return null;
+    }
+
+    public boolean deleteStudentById(Long id) {
+        return true;
+    }
+
+    public AccentureStudent updateStudent(AccentureStudent student) {
+        return null;
+    }
+}
