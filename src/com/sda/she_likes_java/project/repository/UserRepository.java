bb31989bfa6cbc3b82bd6ba74884fbd40d8eeaaa
petko <petkova153@gmail.com>
2023-06-07 19:56:56 +0300

    discover phase

---
+++ src/com/sda/she_likes_java/project/repository/UserRepository.java
@@ -0,0 +1,32 @@
+package com.sda.she_likes_java.project.repository;
+
+import com.sda.she_likes_java.project.entity.User;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class UserRepository {
+    public void registerNewUser(){
+
+    }
+
+    public void deleteUser(){
+
+    }
+
+    public List<User> listUsers(){
+        return List.of();
+    }
+
+    public void updateUser(User user){
+
+    }
+
+    public List<User> getUsersBasedOnCriteria(){
+        return List.of();
+    }
+
+    public List<User> getOrderedUsers(Comparator<User> comparator) {
+        return List.of();
+    }
+}
